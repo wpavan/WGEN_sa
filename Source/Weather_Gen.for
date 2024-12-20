96f269d6fe552327e0352c09df18cbdc5eda2bcf
Virginia Covert <virginiacovert454@gmail.com>
2024-12-20 17:03:54 +0100

    Working WGEN_sa.exe file w/o documentation.

--- Source/Weather_Gen.for
+++ Source/Weather_Gen.for
@@ -32,35 +32,40 @@
 !-----------------------------------------------------------------------
       USE ModuleDefs   
       IMPLICIT NONE
+      EXTERNAL CALC_TDEW, HMET, SOLAR, YR_DOY
       SAVE
 
       CHARACTER*1  MEWTH    !, RNMODE
       CHARACTER*4  INSI
+      CHARACTER*6  RHCOLUMNNAME
 !      CHARACTER*6  ERRKEY
-      CHARACTER*12 FILEW, CLIFILE, Text
+      CHARACTER*80 FILEW, CLIFILE, Text
 !      CHARACTER*78 MESSAGE(10)
 !      CHARACTER*80 PATHWT
 !      CHARACTER*92 FILEWW
 
-      INTEGER YRDOY, YRSIM
+      INTEGER YR, YRDOY, YRSIM
       INTEGER RSEED1, RSEED(4)
       INTEGER DYNAMIC
       INTEGER INCDAT, LENGTH, LenString
-
+      INTEGER RHTHRESHOLD, RHHOURSOVER, HR
       INTEGER StartDate, EndDate
 
-!      INTEGER DOY, YEAR, YRDOY, YRSIM, YYYYDDD
-!      INTEGER RSEED1, RSEED(4), REPNO
-!      INTEGER DYNAMIC, YREND
+      INTEGER DOY
 
       REAL
-     &  CCO2, CO2, PAR, 
-     &  PI, RAD, RAIN, REFHT, RHUM, SRAD, 
-     &  TAMP, TAV,  
-     &  TMAX, TMIN, WINDHT, XELEV, XLAT, XLONG
+     &  CCO2, CLOUDS, CO2, DAYL, DEC, ISINB, PAR, 
+     &  PI, RAD, RAIN, REFHT, RHUM, 
+     &  S0N, SNDN, SNUP, SRAD, 
+     &  TAMP, TAV, TAVG, TDAY, TDEW, TGROAV, TGRODY,
+     &  TMAX, TMIN, WINDHT, WINDSP,
+     &  XELEV, XLAT, XLONG
+     
+      LOGICAL NOTDEW
+      REAL CALC_TDEW
 
-!      REAL, DIMENSION(TS) :: AMTRH, AZZON, BETA, FRDIFP, FRDIFR, PARHR
-!      REAL, DIMENSION(TS) :: RADHR, RHUMHR, TAIRHR, TGRO, WINDHR
+      REAL, DIMENSION(TS) :: AMTRH, AZZON, BETA, FRDIFP, FRDIFR, PARHR
+      REAL, DIMENSION(TS) :: RADHR, RHUMHR, TAIRHR, TGRO, WINDHR
 
 !      PARAMETER (ERRKEY = 'WEATHR')
       PARAMETER (PI=3.14159, RAD=2.0*PI/365.0)
@@ -94,6 +99,11 @@
         READ(Text, '(I7)') RSEED1
         CALL GETARG(4, MEWTH)
         CALL GETARG(5, CLIFILE)
+        CALL GETARG(6, Text)
+        READ(Text, '(I7)') RHTHRESHOLD
+
+        LENGTH = LENSTRING(Text)
+        RHCOLUMNNAME = "   "(1:(3-LENGTH)) // "RH" // Text
 
         LENGTH = LENSTRING(CLIFILE)
         FILEW = CLIFILE(1:LENGTH-4) // ".WTG"
@@ -126,7 +136,12 @@ C       Set default values FOR REFHT AND WINDHT
           ENDIF
 
 !     Subroutine to determine daily CO2
-      CALL CO2VAL(DYNAMIC, YRSIM, YRDOY, CO2)
+!      CALL CO2VAL(DYNAMIC, YRSIM, YRDOY, CO2)
+
+C     Calculate daily solar parameters.
+      CALL SOLAR(
+     &    DAYL, DEC, SRAD, XLAT,                          !Input
+     &    CLOUDS, ISINB, S0N)                             !Output
 
 !C     Adjust wind speed from reference height to 2m height.
 !      IF (WINDSP > 0.0) THEN
@@ -136,8 +151,8 @@ C       Set default values FOR REFHT AND WINDHT
 !      ENDIF
 !
       CALL OpWeath(DYNAMIC, YRDOY, FILEW,
-     &    INSI, XLAT, XLONG, XELEV, TAV, TAMP, REFHT, WINDHT,   !Station
-     &    CO2, PAR, RAIN, SRAD, TMAX, TMIN)  !Daily values
+     &    INSI, XLAT, XLONG, XELEV, TAV, TAMP, REFHT, WINDHT,           !Station
+     &    CO2, PAR, RAIN, RHCOLUMNNAME, RHHOURSOVER, SRAD, TMAX, TMIN)  !Daily values
 
       YRDOY = StartDate
       YRSIM = StartDate
@@ -159,22 +174,67 @@ C     Read new weather record.
         TMIN = -99.0
         RAIN = -99.0
         PAR  = -99.0
+        RHHOURSOVER = 0
 
       CALL WGEN(DYNAMIC,
      &      CLIFILE, MEWTH, RSEED1, YRDOY, YRSIM,         !Input
      &      INSI, RSEED, TAMP, TAV, XELEV, XLAT, XLONG,   !Output
      &      PAR, RAIN, SRAD, TMAX, TMIN)                  !Output
 
+!     Update YR and DOY for DAYL calculations
+      CALL YR_DOY(YRDOY, YR, DOY)
+
+C     Calculate day length, sunrise and sunset.
+      CALL DAYLEN(
+     &    DOY, XLAT,                                      !Input
+     &    DAYL, DEC, SNDN, SNUP)                          !Output
+
 !     Subroutine to determine daily CO2
       CALL CO2VAL(DYNAMIC, YRSIM, YRDOY, CO2)
 
-!C     Adjust wind speed from reference height to 2m height.
-!      IF (WINDSP > 0.0) THEN
-!        WINDSP = WINDSP * (2.0 / WINDHT) ** 2.0
-!      ELSE
-!        WINDSP = 86.4
-!      ENDIF
-!
+C     Calculate daily solar parameters.
+      CALL SOLAR(
+     &    DAYL, DEC, SRAD, XLAT,                          !Input
+     &    CLOUDS, ISINB, S0N)                             !Output
+
+C     Adjust wind speed from reference height to 2m height.
+      IF (WINDSP > 0.0) THEN
+        WINDSP = WINDSP * (2.0 / WINDHT) ** 2.0
+      ELSE
+        WINDSP = 86.4
+      ENDIF
+
+!     Substitute default values if TDEW is missing.
+      IF (TDEW <= -90.)  THEN 
+c               MJ, 2007-04-05: set TDEW to TMIN if TDEW not otherwise available.  This is not
+c               appropriate to South African (and presumably other) conditions
+c               --> suggest replacing with a better calculation based on relative humidity, if
+c                   available.
+          NOTDEW = .TRUE.
+          IF (RHUM .GT. 0.01) THEN
+              TDEW = CALC_TDEW(TMIN, RHUM)
+          ELSE
+             TDEW = TMIN
+          ENDIF
+      ELSE
+          NOTDEW = .FALSE.
+      ENDIF  
+
+C     Calculate hourly weather data.
+      CALL HMET(
+     &    CLOUDS, DAYL, DEC, ISINB, PAR, REFHT,           !Input
+     &    SNDN, SNUP, S0N, SRAD, TDEW, TMAX,              !Input
+     &    TMIN, WINDHT, WINDSP, XLAT,                     !Input
+     &    AMTRH, AZZON, BETA, FRDIFP, FRDIFR, PARHR,      !Output
+     &    RADHR, RHUMHR, TAIRHR, TAVG, TDAY, TGRO,        !Output
+     &    TGROAV, TGRODY, WINDHR)                         !Output
+
+      DO HR = 1, TS
+          IF (RHUMHR(HR) .GE. RHTHRESHOLD) THEN
+              RHHOURSOVER = RHHOURSOVER + 1
+          ENDIF
+      ENDDO
+
 !***********************************************************************
 !***********************************************************************
 !     Daily Output
@@ -182,8 +242,8 @@ C     Read new weather record.
       DYNAMIC = 5 
 C-----------------------------------------------------------------------
       CALL OpWeath(DYNAMIC, YRDOY, FILEW,
-     &    INSI, XLAT, XLONG, XELEV, TAV, TAMP, REFHT, WiNDHT,   !Station
-     &    CO2, PAR, RAIN, SRAD, TMAX, TMIN)        !Daily values
+     &    INSI, XLAT, XLONG, XELEV, TAV, TAMP, REFHT, WiNDHT,           !Station
+     &    CO2, PAR, RAIN, RHCOLUMNNAME, RHHOURSOVER, SRAD, TMAX, TMIN)  !Daily values
 
       YRDOY = INCDAT(YRDOY, 1)
 
@@ -193,8 +253,8 @@ C-----------------------------------------------------------------------
       DYNAMIC = 6
 
       CALL OpWeath(DYNAMIC, YRDOY, FILEW,
-     &    INSI, XLAT, XLONG, XELEV, TAV, TAMP, REFHT, WiNDHT,   !Station
-     &    CO2, PAR, RAIN, SRAD, TMAX, TMIN)        !Daily values
+     &    INSI, XLAT, XLONG, XELEV, TAV, TAMP, REFHT, WiNDHT,           !Station
+     &    CO2, PAR, RAIN, RHCOLUMNNAME, RHHOURSOVER, SRAD, TMAX, TMIN)  !Daily values
 
       STOP
       END PROGRAM Weather_Gen
